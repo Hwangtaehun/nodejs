5bb91dc0eab0b4705f5a61fe174b6a5ea9be3a80
Hwangtaehun <r283@naver.com>
2025-09-21 16:21:52 +0900

    express사용해보기

--- nodejs_echo_server/express-server/refactoring-to-express.js
+++ nodejs_echo_server/express-server/refactoring-to-express.js
@@ -3,22 +3,24 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.listen(port, () => console.log("라우터를 리팩터링해보자!"));
+app.listen(port, () => {
+  console.log("익스프레스로 라우터 리팩토링하기");
+});
 
 app.get("/", (_, res) => res.end("HOME"));
 app.get("/user", user);
 app.get("/feed", feed);
 
 function user(req, res) {
-    const userInfo = url.parse(req.url, true).query;
-    res.json(`[user] name : ${userInfo.name}, age: ${userInfo.age}`);
+  const user = url.parse(req.url, true).query;
+  res.json(`[user] name : ${user.name}, age: ${user.age}`);
 }
 
-function feed(_, res){
-    res.json(`<ul>
+function feed(_, res) {
+  res.json(`<ul>
         <li>picture1</li>
         <li>picture2</li>
         <li>picture3</li>
-        </ul>
-    `);
+      </ul>
+      `);
 }
