42a3d4ab25025a1366c4ff282b1eedee135f0b6c
Hwangtaehun <r283@naver.com>
2025-09-23 15:44:34 +0900

    업로드

---
+++ nodejs_asynchronous/callback-promise-async-await/promise-anti-pattern2.js
@@ -0,0 +1,25 @@
+function myWork(work) {
+    return new Promise((resolve, reject) => {
+        resolve(work.toUpperCase())
+    })
+}
+
+function playGame(work) {
+    return new Promise((resolve, reject) => {
+        if(work === 'done'){
+            resolve('GO PLAY GAME');
+        } else {
+            reject(new Error("DON'T"));
+        }
+    })
+}
+
+myWork('done').then(function(result) {
+    playGame(result).then(function(val){
+        console.log(val);
+    });
+})
+
+myWork('done')
+    .then(playGame)
+    .then(console.log);
