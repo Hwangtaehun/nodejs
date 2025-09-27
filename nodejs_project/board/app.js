c3d1ecea3923ef12dde394fcdbd447d5c292acfd
Hwangtaehun <r283@naver.com>
2025-09-27 21:41:20 +0900

    업로드

---
+++ nodejs_project/board/app.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const handlebars = require("express-handlebars");
+const app = express();
+const mongodbConnection = require("./configs/mongodb-connection");
+const postService = require("./services/post-service");
+
+let collection;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.engine("handlebars", handlebars.engine());
+app.set("view engine", "handlebars");
+app.set("veiws", __dirname + "/views");
+
+app.get("/", (req, res) => {
+    res.render("home", { title: "테스트 게시판", message: "만나서 반갑습니다!" });
+});
+
+app.get("/write", (req, res) =>{
+    res.render("write", {title: "테스크 게시판"});
+});
+
+app.post("/write", async (req, res) => {
+    const post = req.body;
+    const result = await postService.writePost(collection, post);
+    res.redirect(`/detail/${result.insertedId}`);
+});
+
+app.get("/detail/:id", async(req, res) => {
+    res.render("detail", {
+        title: "테스트 게시판",
+    });
+});
+
+app.get("/", async (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const search = req.query.search || "";
+    try{
+        const[posts, paginator] = await postService.list(collection, page, search);
+        res.render("home", {title: "테스트 게시판", search, paginator, posts});
+    } catch (error) {
+        console.error(error);
+        res.render("home", {title: "테스트 게시판"});
+    }
+});
+
+app.listen(3000, async() => {
+    console.log("Server started");
+    const MongoClient = await mongodbConnection();
+    collection = MongoClient.db().collection("post");
+    console.log("MongoDB connected");
+});
+
+app.engine(
+    "handlebars",
+    handlebars.create({
+        helpers: require("./configs/handlebars-helpers"),
+    }).engine,
+);
