dd173a59e99696cd25057d690ba6fe41bf26425f
Hwangtaehun <r283@naver.com>
2025-09-28 20:29:56 +0900

    수정 중
    post.createdDt 오타

--- nodejs_project/board/app.js
+++ nodejs_project/board/app.js
@@ -1,17 +1,35 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
-const app = express();
 const mongodbConnection = require("./configs/mongodb-connection");
 const postService = require("./services/post-service");
 
 let collection;
 
+const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.engine("handlebars", handlebars.engine());
+app.engine(
+    "handlebars",
+    handlebars.create({
+        helpers: require("./configs/handlebars-helpers"),
+    }).engine,
+);
+
 app.set("view engine", "handlebars");
 app.set("veiws", __dirname + "/views");
+
+app.get("/", async (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const search = req.query.search || "";
+    try{
+        const[posts, paginator] = await postService.list(collection, page, search);
+        res.render("home", {title: "테스트 게시판", search, paginator, posts});
+    } catch (error) {
+        console.error(error);
+        res.render("home", {title: "테스트 게시판"});
+    }
+});
 
 app.get("/write", (req, res) =>{
     res.render("write", {title: "테스크 게시판", mode: "create"});
@@ -21,6 +39,25 @@ app.post("/write", async (req, res) => {
     const post = req.body;
     const result = await postService.writePost(collection, post);
     res.redirect(`/detail/${result.insertedId}`);
+});
+
+app.get("/detail/:id", async(req, res) => {
+    const result = await postService.getDetailPost(collection, req.params.id);
+    res.render("detail", {
+        title: "테스트 게시판",
+        post: result.value,
+    });
+});
+
+app.post("/check-password", async (req, res) => {
+    const { id, password } = req.body;
+    const post = postService.getPostByIdAndPassword(collection, { id, password });
+
+    if (!post) {
+        return res.status(404).json({ isExist: false });
+    } else {
+        return res.json({ isExist: true });
+    }
 });
 
 app.get("/modify/:id", async (req, res) =>{
@@ -37,36 +74,9 @@ app.post("/modify/", async(req, res) => {
     res.redirect(`/detail/${id}`);
 });
 
-app.get("/detail/:id", async(req, res) => {
-    const result = await postService.getDetailPost(collection, req.params.id);
-    res.render("detail", {
-        title: "테스트 게시판",
-        post: result.value,
-    });
-});
-
-app.get("/", async (req, res) => {
-    const page = parseInt(req.query.page) || 1;
-    const search = req.query.search || "";
-    try{
-        const[posts, paginator] = await postService.list(collection, page, search);
-        res.render("home", {title: "테스트 게시판", search, paginator, posts});
-    } catch (error) {
-        console.error(error);
-        res.render("home", {title: "테스트 게시판"});
-    }
-});
-
 app.listen(3000, async() => {
     console.log("Server started");
     const MongoClient = await mongodbConnection();
     collection = MongoClient.db().collection("post");
     console.log("MongoDB connected");
 });
-
-app.engine(
-    "handlebars",
-    handlebars.create({
-        helpers: require("./configs/handlebars-helpers"),
-    }).engine,
-);
