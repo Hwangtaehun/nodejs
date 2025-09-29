a64cf817829af5503efdc0761781c235336ecacf
Hwangtaehun <r283@naver.com>
2025-09-29 21:41:58 +0900

    비동기 완료

--- nodejs_nestjs_start/blog/src/blog.repository.ts
+++ nodejs_nestjs_start/blog/src/blog.repository.ts
@@ -1,48 +1,72 @@
+import { Injectable } from '@nestjs/common';
 import { readFile, writeFile } from 'fs/promises';
 import { PostDto } from './blog.model';
 
 export interface BlogRepository {
   getAllPost(): Promise<PostDto[]>;
-  createPost(postDto: PostDto);
-  getPost(id: string): Promise<PostDto>;
-  deletePost(id: string);
-  updatePost(id: string, postDto: PostDto);
+  createPost(
+    postDto: Omit<PostDto, 'id' | 'createdDt' | 'updatedDt'>,
+  ): Promise<PostDto>;
+  getPost(id: string): Promise<PostDto | undefined>;
+  deletePost(id: string): Promise<void>;
+  updatePost(
+    id: string,
+    postDto: Omit<PostDto, 'id' | 'createdDt'>,
+  ): Promise<PostDto | undefined>;
 }
 
+@Injectable()
 export class BlogFileRepository implements BlogRepository {
   FILE_NAME = './src/blog.data.json';
 
   async getAllPost(): Promise<PostDto[]> {
     const datas = await readFile(this.FILE_NAME, 'utf8');
-    const posts = JSON.parse(datas);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const posts: PostDto[] = JSON.parse(datas);
     return posts;
   }
 
-  async createPost(postDto: PostDto) {
+  async createPost(
+    postDto: Omit<PostDto, 'id' | 'createdDt' | 'updatedDt'>,
+  ): Promise<PostDto> {
     const posts = await this.getAllPost();
-    const id = posts.length + 1;
-    const createPost = { id: id.toString(), ...postDto, createdDt: new Date() };
+    const id = (posts.length + 1).toString();
+    const createPost: PostDto = {
+      id,
+      ...postDto,
+      createdDt: new Date(),
+    };
     posts.push(createPost);
-    await writeFile(this.FILE_NAME, JSON.stringify(posts));
+    await writeFile(this.FILE_NAME, JSON.stringify(posts, null, 2));
+    return createPost;
   }
 
-  async getPost(id: string): Promise<PostDto> {
+  async getPost(id: string): Promise<PostDto | undefined> {
     const posts = await this.getAllPost();
-    const result = posts.find((post) => post.id === id);
-    return result;
+    return posts.find((post) => post.id === id);
   }
 
-  async deletePost(id: string) {
+  async deletePost(id: string): Promise<void> {
     const posts = await this.getAllPost();
     const filteredPosts = posts.filter((post) => post.id !== id);
-    await writeFile(this.FILE_NAME, JSON.stringify(filteredPosts));
+    await writeFile(this.FILE_NAME, JSON.stringify(filteredPosts, null, 2));
   }
 
-  async updatePost(id: string, postDto: PostDto) {
+  async updatePost(
+    id: string,
+    postDto: Omit<PostDto, 'id' | 'createdDt'>,
+  ): Promise<PostDto | undefined> {
     const posts = await this.getAllPost();
     const index = posts.findIndex((post) => post.id === id);
-    const updatePost = { id, ...postDto, updateDt: new Date() };
+    if (index === -1) return undefined;
+
+    const updatePost: PostDto = {
+      ...posts[index],
+      ...postDto,
+      updatedDt: new Date(),
+    };
     posts[index] = updatePost;
-    await writeFile(this.FILE_NAME, JSON.stringify(posts));
+    await writeFile(this.FILE_NAME, JSON.stringify(posts, null, 2));
+    return updatePost;
   }
 }
--- nodejs_nestjs_start/blog/src/blog.service.ts
+++ nodejs_nestjs_start/blog/src/blog.service.ts
@@ -1,42 +1,32 @@
+import { Injectable } from '@nestjs/common';
 import { PostDto } from './blog.model';
+import { BlogFileRepository, BlogRepository } from './blog.repository';
 
 export class BlogService {
-  private posts: PostDto[] = [];
+  //private posts: PostDto[] = [];
+  blogRespository: BlogRepository;
 
-  getAllPosts(): PostDto[] {
-    return this.posts;
+  constructor() {
+    this.blogRespository = new BlogFileRepository();
   }
 
-  createPost(postData: Omit<PostDto, 'id' | 'createdDt' | 'updatedDt'>): PostDto {
-    const id = (this.posts.length + 1).toString();
-    const newPost: PostDto = {
-      id,
-      ...postData,
-      createdDt: new Date(),
-    };
-    this.posts.push(newPost);
-    return newPost;
+  async getAllPosts() {
+    return await this.blogRespository.getAllPost();
   }
 
-  getPost(id: string): PostDto | undefined {
-    return this.posts.find((post) => post.id === id);
+  createPost(postDto: PostDto) {
+    this.blogRespository.createPost(postDto);
+  }
+
+  async getPost(id: string): Promise<PostDto | undefined> {
+    return await this.blogRespository.getPost(id);
   }
 
   delete(id: string): void {
-    this.posts = this.posts.filter((post) => post.id !== id);
+    this.blogRespository.deletePost(id);
   }
 
-  updatePost(id: string, postData: Omit<PostDto, 'id' | 'createdDt'>): PostDto | undefined {
-    const updateIndex = this.posts.findIndex((post) => post.id === id);
-    if (updateIndex === -1) return undefined;
-
-    const updatePost: PostDto = {
-      ...this.posts[updateIndex],
-      ...postData,
-      updatedDt: new Date(),
-    };
-
-    this.posts[updateIndex] = updatePost;
-    return updatePost;
+  updatePost(id, postDto: PostDto) {
+    this.blogRespository.updatePost(id, postDto);
   }
 }
