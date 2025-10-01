7051ae109c1c47f83aa3c2d562e5688fe99c5502
Hwangtaehun <r283@naver.com>
2025-10-01 21:47:34 +0900

    쿠키 생성
    가드를 사용해 인증됐는지 검사 부터 하기 404쪽

--- nodejs_authentication/nest-auth-test/src/auth/auth.controller.ts
+++ nodejs_authentication/nest-auth-test/src/auth/auth.controller.ts
@@ -1,4 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { Body, Controller, Post, Request, Response } from '@nestjs/common';
 import { CreateUseDto } from 'src/user/user.dto';
 import { AuthService } from './auth.service';
 
@@ -9,5 +12,21 @@ export class AuthController {
   @Post('register')
   async register(@Body() userDto: CreateUseDto) {
     return await this.authService.register(userDto);
+  }
+
+  @Post('login')
+  async login(@Request() req, @Response() res) {
+    const userInfo = await this.authService.validateUser(
+      req.body.email,
+      req.body.password,
+    );
+
+    if (userInfo) {
+      res.cookie('login', JSON.stringify(userInfo), {
+        httpOnly: false,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+      });
+    }
+    return res.send({ message: 'login success' });
   }
 }
