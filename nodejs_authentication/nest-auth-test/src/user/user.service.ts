a3be4d369b91ffd4837df1bf8015a18aaf72cde2
Hwangtaehun <r283@naver.com>
2025-10-01 20:33:05 +0900

    유저 모듈의 엔티티, 서비스, 컨트롤러 생성

---
+++ nodejs_authentication/nest-auth-test/src/user/user.service.ts
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  createUser(user: User): Promise<User> {
+    return this.userRepository.save(user);
+  }
+
+  async getUser(email: string) {
+    const result = await this.userRepository.findOne({
+      where: { email },
+    });
+    return result;
+  }
+
+  async updateUser(email: string, _user: User) {
+    const user = (await this.getUser(email)) as User; //이 부분 주의
+    console.log(_user);
+    user.username = _user.username;
+    user.password = _user.password;
+    console.log(user);
+    this.userRepository.save(user);
+  }
+
+  deleteUser(email: string) {
+    return this.userRepository.delete({ email });
+  }
+}
