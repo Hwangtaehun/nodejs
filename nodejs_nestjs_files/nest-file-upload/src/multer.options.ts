245884dd38416ba1ce0fce9f96d3a17446521592
Hwangtaehun <r283@naver.com>
2025-10-04 20:44:07 +0900

    파일 업로드 API 만들기

--- nodejs_nestjs_files/nest-file-upload/src/multer.options.ts
+++ nodejs_nestjs_files/nest-file-upload/src/multer.options.ts
@@ -3,7 +3,7 @@ import { diskStorage } from 'multer';
 import { extname, join } from 'path';
 
 export const multerOption = {
-  Storage: diskStorage({
+  storage: diskStorage({
     destination: join(__dirname, '..', 'uploads'),
     filename: (req, file, cb) => {
       cb(null, randomUUID() + extname(file.originalname));
