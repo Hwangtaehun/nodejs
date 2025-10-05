c9e289e0ce059421c1d05d067ed83a32ed6f0f4b
Hwangtaehun <r283@naver.com>
2025-10-05 21:22:53 +0900

    웹 채팅 프로그램 완성

--- nodejs_chat/nest-chat/src/app.gateway.ts
+++ nodejs_chat/nest-chat/src/app.gateway.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import {
   WebSocketGateway,
@@ -21,13 +20,38 @@ export class ChatGateway {
 
 @WebSocketGateway({ namespace: 'room' })
 export class RoomGateway {
+  constructor(private readonly chatGateway: ChatGateway) {}
   rooms: string[] = [];
+
   @WebSocketServer()
   server: Server;
+
   @SubscribeMessage('createRoom')
   handleMessage(@MessageBody() data) {
     const { nickname, room } = data;
+    this.chatGateway.server.emit('notice', {
+      message: `${nickname}님이 ${room}방을 만들었습니다. `,
+    });
     this.rooms.push(room);
     this.server.emit('rooms', this.rooms);
+  }
+
+  @SubscribeMessage('joinRoom')
+  handleJoinRoom(socket: Socket, data) {
+    const { nickname, room, toLeaveRoom } = data;
+    socket.leave(toLeaveRoom);
+    this.chatGateway.server.emit('notice', {
+      message: `${nickname}님이 ${room}방에 입장했습니다. `,
+    });
+    socket.join(room);
+  }
+
+  @SubscribeMessage('message')
+  handleMessageToRoom(socket: Socket, data){
+    const { nickname, room, message } = data;
+    console.log(data);
+    socket.broadcast.to(room).emit('message', {
+      message: `${nickname}: ${message}`,
+    });
   }
 }
